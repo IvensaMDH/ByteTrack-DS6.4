6005b544831337dfc64ded7dc78bf0eec7bd0b6e
MathiasDall <56653296+IvensaMDH@users.noreply.github.com>
2025-01-28 10:56:42 +0100

    Update BYTETracker.cpp

--- src/BYTETracker.cpp
+++ src/BYTETracker.cpp
@@ -1,8 +1,8 @@
 #include "BYTETracker.h"
 
 BYTETracker::BYTETracker(int frame_rate, int track_buffer) {
-    track_thresh  = 0.2; //low-confidence selection threshold, below which the detection box is not tracked.
-    high_thresh   = 0.9; 
+    track_thresh  = 0.15; //low-confidence selection threshold, below which the detection box is not tracked.
+    high_thresh   = 0.925; 
     match_thresh  = 0.99; //matching threshold for the IOU of the previous frame and the second frame, if there is a (1-IOU) > match_thresh, the direct tracking fails.
     frame_id      = 0;
     max_time_lost = 15;
